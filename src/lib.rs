53253a8e7ab64b674f48620723601ff12b9e052b
Avi ד <avi.the.coder@gmail.com>
2018-11-02 08:39:36 -0400

    Add serde (de)serialization and Tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,53 @@
-use std::ops::Range;
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+// use std::ops::Range;
 
 /// A parser for the output of [grep_printer::JSON](https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html).
 /// Created to deserialize `ripgrep` `--json` output for [rg_replace](https://github.com/Avi-D-coder/rg_replace).
-
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "lowercase")]
+#[serde(tag = "type", content = "data")]
 pub enum Type {
     /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-begin
-    Begin(ArbitraryData),
-    End,
-    Match,
-    Context,
+    Begin { path: ArbitraryData },
+    /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-end
+    End {
+        path: ArbitraryData,
+        binary_offset: Option<isize>, // FIXME I am not sure it's a isize
+        stats: Stats,
+    },
+    /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-match
+    Match {
+        path: ArbitraryData,
+        lines: ArbitraryData,
+        line_number: Option<usize>,
+        absolute_offset: isize,
+        submatches: Vec<Submatch>, //TODO Optimize
+    },
+    /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-context
+    Context {
+        path: ArbitraryData,
+        lines: ArbitraryData,
+        line_number: Option<usize>,
+        absolute_offset: isize,
+        submatches: Vec<Submatch>,
+    },
 }
 
 /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#object-arbitrary-data
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+#[serde(untagged)]
 pub enum ArbitraryData {
-    Text(String),
-    Base64(String),
-}
-
-/// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-end
-pub struct End {
-    pub path: ArbitraryData,
-    pub binary_offset: Option<isize>, // FIXME I am not sure it's a isize
-    pub stats: Stats,
+    Text { text: String },
+    Base64 { bytes: String },
 }
 
 /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#object-stats
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 pub struct Stats {
     pub elapsed: Duration,
     pub searches: isize,
@@ -36,42 +59,127 @@ pub struct Stats {
 }
 
 /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#object-duration
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 pub struct Duration {
     pub secs: usize,
     pub nanos: usize,
-    pub human: f64,
-}
-
-/// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-match
-pub struct Match {
-    pub path: ArbitraryData,
-    pub lines: ArbitraryData,
-    pub line_number: Option<usize>,
-    pub absolute_offset: isize,
-    pub submatches: Vec<Submatch>, //TODO Optimize
+    pub human: String,
 }
 
 /// Almost as specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#object-submatch
 /// `match` is deserialized to `matched` because a rust reserves match as a keyword.
 /// `start` and `end` are also deserialized to `range`.
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
+#[serde(rename = "submatch")]
 pub struct Submatch {
+    #[serde(rename = "match")]
     pub matched: ArbitraryData,
-    pub range: Range<usize>,
-}
-
-/// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-context
-pub struct Context {
-    pub path: ArbitraryData,
-    pub lines: ArbitraryData,
-    pub line_number: Option<usize>,
-    pub absolute_offset: isize,
-    pub submatches: Vec<Submatch>,
+    pub start: usize,
+    pub end: usize,
+    // pub range: Range<usize>,
 }
 
 #[cfg(test)]
 mod tests {
+    // tests based on [`grep_printer` example output](https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#example)
+
+    use crate::{ArbitraryData::*, Type::*, *};
+
     #[test]
-    fn it_works() {
-        unimplemented!();
+    fn arbitrarydata() {
+        let json = r#"{"text":"/home/andrew/sherlock"}"#;
+        assert_eq!(
+            Text {
+                text: "/home/andrew/sherlock".to_owned()
+            },
+            serde_json::from_str(json).unwrap()
+        )
     }
+
+    #[test]
+    fn begin_deserialize() {
+        let json = r#"{"type":"begin","data":{"path":{"text":"/home/andrew/sherlock"}}}"#;
+        assert_eq!(
+            Begin {
+                path: Text {
+                    text: "/home/andrew/sherlock".to_owned()
+                }
+            },
+            serde_json::from_str(json).unwrap()
+        );
+    }
+
+    #[test]
+    fn end_deserialize() {
+        let json=r#"{"type":"end","data":{"path":{"text":"/home/andrew/sherlock"},"binary_offset":null,"stats":{"elapsed":{"secs":0,"nanos":36296,"human":"0.0000s"},"searches":1,"searches_with_match":1,"bytes_searched":367,"bytes_printed":1151,"matched_lines":2,"matches":2}}}"#;
+        assert_eq!(
+            End {
+                path: Text {
+                    text: "/home/andrew/sherlock".to_owned()
+                },
+                binary_offset: None,
+                stats: Stats {
+                    elapsed: Duration {
+                        secs: 0,
+                        nanos: 36296,
+                        human: "0.0000s".to_owned()
+                    },
+                    searches: 1,
+                    searches_with_match: 1,
+                    bytes_searched: 367,
+                    bytes_printed: 1151,
+                    matched_lines: 2,
+                    matches: 2
+                }
+            },
+            serde_json::from_str(json).unwrap()
+        );
+    }
+
+    #[test]
+    fn match_deserialize() {
+        let json=r#"{"type":"match","data":{"path":{"text":"/home/andrew/sherlock"},"lines":{"text":"but Doctor Watson has to have it taken out for him and dusted,\n"},"line_number":5,"absolute_offset":258,"submatches":[{"match":{"text":"Watson"},"start":11,"end":17}]}}"#;
+        assert_eq!(
+            Match {
+                path: Text {
+                    text: "/home/andrew/sherlock".to_owned()
+                },
+                lines: Text {
+                    text: "but Doctor Watson has to have it taken out for him and dusted,\n"
+                        .to_owned()
+                },
+                line_number: Some(5),
+                absolute_offset: 258,
+                submatches: vec![Submatch {
+                    matched: Text {
+                        text: "Watson".to_owned()
+                    },
+                    start: 11,
+                    end: 17
+                }],
+            },
+            serde_json::from_str(json).unwrap()
+        )
+    }
+
+    #[test]
+    fn content_deserialize() {
+        let json = r#"{"type":"context","data":{"path":{"text":"/home/andrew/sherlock"},"lines":{"text":"can extract a clew from a wisp of straw or a flake of cigar ash;\n"},"line_number":4,"absolute_offset":193,"submatches":[]}}"#;
+        assert_eq!(
+            Context {
+                path: Text {
+                    text: "/home/andrew/sherlock".to_owned()
+                },
+                lines: Text {
+                    text: "can extract a clew from a wisp of straw or a flake of cigar ash;\n"
+                        .to_owned()
+                },
+                line_number: Some(4),
+                absolute_offset: 193,
+                submatches: vec![],
+            },
+            serde_json::from_str(json).unwrap()
+        )
+    }
+
 }
