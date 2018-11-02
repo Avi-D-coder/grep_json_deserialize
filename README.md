ea823818b22d581def750e32967994dcc4798c8f
Avi ד <avi.the.coder@gmail.com>
2018-11-01 22:51:29 -0400

    Added README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+#### A parser for the output of [grep_printer::JSON](https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html).
+Created to deserialize `ripgrep` `--json` output for [rg_replace](https://github.com/Avi-D-coder/rg_replace).
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::ops::Range;
 
 /// A parser for the output of [grep_printer::JSON](https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html).
-///
+/// Created to deserialize `ripgrep` `--json` output for [rg_replace](https://github.com/Avi-D-coder/rg_replace).
 
 pub enum Type {
     /// As specified in: https://docs.rs/grep-printer/0.1.1/grep_printer/struct.JSON.html#message-begin
